ea6bda24727091299cfb720debf1b803acd0ee6b
Gao Yan Long <gyanlon@hotmail.com>
2018-06-13 11:32:07 +0800

    show back in search box

--- src/page_search.py
+++ src/page_search.py
@@ -6,7 +6,7 @@ import json
 def search_form():
     return '''
         <form action="/search" method="post">
-            <input name="querystr" type="text" maxlength="255" style="height:34;width:540"/>
+            <input name='querystr' type="text" maxlength="255" style="height:34;width:540"/>
             <input value="查查看" type="submit" style="height:34;width:100;background:#ddd;font-size:16"/>
         </form>
     '''
@@ -15,7 +15,7 @@ def search_form():
 def do_search():
     querystr = request.forms.get('querystr')
     records = query(querystr)
-    searchform = search_form()
+    searchform = search_form().replace("input name='querystr'", "input name='querystr' value='" + querystr + "'")
     for record in records :
       searchform = searchform + "<div style='border-bottom:medium solid #ddd;border-width: 0 0 1 0; padding-bottom: 5px'>%s</div>" % record
 
