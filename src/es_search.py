7810fc88aa76d463129e8a5a7defc773eaa2fe6c
Gao Yan Long <gyanlon@hotmail.com>
2018-06-22 11:57:04 +0800

    Added highlight

--- src/es_search.py
+++ src/es_search.py
@@ -6,20 +6,30 @@ import cgi
 import constants
 import logging
 
-es = Elasticsearch()
-s = Search(using=es)
+es = Elasticsearch([{"host":constants.ES_IP,"port":constants.ES_PORT}])
 
 def query(querystr):    
     records = []
     logging.info("search:%s" % querystr)
-    response = requests.get("http://{}:{}/_search?q=content_body:{}".format(constants.ES_IP, constants.ES_PORT, querystr))
-    res = json.loads(response.text)
+    # response = requests.get("http://{}:{}/_search?q=content_body:{}".format(constants.ES_IP, constants.ES_PORT, querystr))
+    # res = json.loads(response.text)
 
-    for hit in res['hits']['hits']:
-        records.append(hit["_source"])
+    s = Search(using=es, index="doc") \
+        .query("match", content_body=querystr) \
+        .highlight("content_body", fragment_size=2048, number_of_fragments=50) \
+        .highlight_options(pre_tags=["<em style='color:blue;font:bold;'>"], post_tags=["</em>"])
+
+    response = s.execute()
+
+    for hit in response:
+        for fragment in hit.meta.highlight.content_body:
+            records.append(fragment)
+
+    # for hit in response :
+    #     records.append(hit.content_body)
 
     return records
 
 if __name__ == '__main__':
-    logging.info(query('13-Isopropylpodocarpa-7'))
-    logging.info(len(query('13-Isopropylpodocarpa-7')))
+    print(query('13-Isopropylpodocarpa-7'))
+    print(len(query('13-Isopropylpodocarpa-7')))
