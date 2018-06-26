3e25920450ac089e8af6c7640cdafcc7888aeb5b
Gao Yan Long <gyanlon@hotmail.com>
2018-06-26 14:27:05 +0800

    Used bottle template

--- src/page_search.py
+++ src/page_search.py
@@ -5,15 +5,7 @@ import json
 
 @get('/')
 def search_form():
-        return '''
-        <head>
-        <meta http-equiv="content-type" content="text/html;charset=unicode">
-        </head>
-        <form action="/search" method="post">
-            <input name='querystr' type="text" maxlength="255" style="height:34;width:540"/>
-            <input value="查查看" type="submit" style="height:34;width:100;background:#ddd;font-size:16"/>
-        </form>
-    '''
+    return template('page_search')
 
 @post('/search') # or @route('/login', method='POST')
 def do_search():
@@ -21,8 +13,4 @@ def do_search():
     print("search : ", querystr)
     
     records = query(querystr)
-    searchform = search_form().replace("input name='querystr'", "input name='querystr' value='" + querystr + "'") #show back query string
-    for record in records :
-      searchform = searchform + "<div style='border-bottom:medium solid #ddd;border-width: 0 0 1 0; padding-bottom: 5px'>%s</div>" % record
-
-    return searchform
+    return template('page_search_results', records=records)
---
+++ src/views/page_search.tpl
@@ -0,0 +1,7 @@
+<head>
+<meta http-equiv="content-type" content="text/html;charset=unicode">
+</head>
+<form action="/search" method="post">
+    <input name='querystr' type="text" maxlength="255" style="height:34;width:540"/>
+    <input value="查查看" type="submit" style="height:34;width:100;background:#ddd;font-size:16"/>
+</form>
